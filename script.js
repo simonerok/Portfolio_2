1bb90d5355f5d88fa0ed304ebdf55feb9337f600
Simone <ssimone12@gmail.com>
2023-03-26 11:26:18 +0200

    smiley added

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-/* DARK MODE */
+window.addEventListener("DOMContentLoaded", init);
+
+/* HOVER SMILEY */
+function init() {
+  document.querySelector("#right-eye").addEventListener("mouseover", onOver);
+  document.querySelector("#right-eye").addEventListener("mouseout", onOut);
+  /*   document.querySelector("#smile").addEventListener("mouseover", moveOver); */
+
+  /* man kan tilføje det på specifikke numre og på hele arrayet med forEach((g, index, array) fx*/
+  document.querySelectorAll("g").forEach((g) => {
+    g.addEventListener("mouseover", onOver);
+    g.addEventListener("mouseout", onOut);
+  });
+
+  function onOver(event) {
+    console.log("over target", event.target);
+    event.target.classList.add("stroke_animation");
+  }
+
+  function onOut(event) {
+    console.log("over target", event.target);
+    event.target.classList.remove("stroke_animation");
+  }
+}
+
+/* DARK MODE/ LIGHT MODE */
 
 /* CHANGE TO DARK */
 let theme = localStorage.getItem("data-theme"); // dette er taget fra html (OVER HEADEREN)
@@ -24,20 +49,18 @@ checkbox.addEventListener("change", () => {
     changeThemeToDark();
     swapImage();
   }
-
 });
-
 
 /* SWAP HEADER IMAGE */
 
 function swapImage() {
-  const imgElement = document.querySelector('.header_graphics object');
+  const imgElement = document.querySelector(".header_graphics object");
   let newImageUrl;
 
-  if (document.documentElement.getAttribute('data-theme') === 'dark') {
-    newImageUrl = 'graphics/light-image.png';
+  if (document.documentElement.getAttribute("data-theme") === "dark") {
+    newImageUrl = "graphics/light-image.png";
   } else {
-    newImageUrl = 'graphics/dark-image.svg';
+    newImageUrl = "graphics/dark-image.svg";
   }
 
   imgElement.data = newImageUrl;
